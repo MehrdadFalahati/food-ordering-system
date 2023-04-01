ebce8b27fbdd40a321dde1de12564df5beb7daed
MehrdadFalahati <m.falahati2022@gmail.com>
2023-04-01 14:08:22 +0430

    feat: implemented message listener

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantapproval/RestaurantApprovalResponseMessageListener.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantapproval/RestaurantApprovalResponseMessageListener.java
@@ -1,4 +1,4 @@
-package com.food.ordering.system.order.service.domain.ports.input.message.listener.payment;
+package com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval;
 
 import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
 
