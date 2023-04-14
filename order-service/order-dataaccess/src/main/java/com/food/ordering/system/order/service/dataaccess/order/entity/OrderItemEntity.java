86b8f3856e4872ffcffd252f6fad03d595d0d1bd
MehrdadFalahati <m.falahati2022@gmail.com>
2023-04-14 17:26:44 +0430

    feat: implemented order data access entity

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderItemEntity.java
@@ -0,0 +1,43 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(OrderItemEntityId.class)
+@Table(name = "order_items")
+@Entity
+public class OrderItemEntity {
+    @Id
+    private Long id;
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ORDER_ID")
+    private OrderEntity order;
+
+    private UUID productId;
+    private BigDecimal price;
+    private Integer quantity;
+    private BigDecimal subtotal;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemEntity that = (OrderItemEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(order, that.order);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order);
+    }
+}
