88e110d89c35deee9830634977cd440264fe9786
MehrdadFalahati <m.falahati2022@gmail.com>
2023-05-23 14:00:51 +0330

    feat: implemented Payment Request Helper class

---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/PaymentRequestHelper.java
@@ -0,0 +1,97 @@
+package com.food.ordering.system.payment.service.domain;
+
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import com.food.ordering.system.domain.valueobject.PaymentStatus;
+import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
+import com.food.ordering.system.payment.service.domain.entity.CreditEntry;
+import com.food.ordering.system.payment.service.domain.entity.CreditHistory;
+import com.food.ordering.system.payment.service.domain.entity.Payment;
+import com.food.ordering.system.payment.service.domain.event.PaymentEvent;
+import com.food.ordering.system.payment.service.domain.exception.PaymentApplicationServiceException;
+import com.food.ordering.system.payment.service.domain.mapper.PaymentDataMapper;
+import com.food.ordering.system.payment.service.domain.port.output.repository.CreditEntryRepository;
+import com.food.ordering.system.payment.service.domain.port.output.repository.CreditHistoryRepository;
+import com.food.ordering.system.payment.service.domain.port.output.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PaymentRequestHelper {
+
+    private final PaymentDomainService paymentDomainService;
+    private final PaymentDataMapper paymentDataMapper;
+    private final PaymentRepository paymentRepository;
+    private final CreditEntryRepository creditEntryRepository;
+    private final CreditHistoryRepository creditHistoryRepository;
+
+    @Transactional
+    public PaymentEvent persistPayment(PaymentRequest paymentRequest) {
+        log.info("Received payment complete event for order id: {}", paymentRequest.getOrderId());
+        Payment payment = paymentDataMapper.paymentRequestModelToPayment(paymentRequest);
+        return getPaymentEvent(payment, PaymentStatus.COMPLETED);
+    }
+
+    @Transactional
+    public PaymentEvent persistCancelPayment(PaymentRequest paymentRequest) {
+        log.info("Received payment rollback event for order id: {}", paymentRequest.getOrderId());
+        Optional<Payment> paymentResponse = paymentRepository
+                .findByOrderId(UUID.fromString(paymentRequest.getOrderId()));
+        if (paymentResponse.isEmpty()) {
+            log.error("Payment with order id: {} could not be found!", paymentRequest.getOrderId());
+            throw new PaymentApplicationServiceException("Payment with order id: " + paymentRequest.getOrderId() + "could not be found!");
+        }
+        Payment payment = paymentResponse.get();
+        return getPaymentEvent(payment, PaymentStatus.CANCELLED);
+    }
+
+    private PaymentEvent getPaymentEvent(Payment payment, PaymentStatus paymentStatus) {
+        CreditEntry creditEntry = getCreditEntry(payment.getCustomerId());
+        List<CreditHistory> creditHistories = getCreditHistories(payment.getCustomerId());
+        List<String> failureMessages = new ArrayList<>();
+        PaymentEvent paymentEvent = null;
+        if (paymentStatus == PaymentStatus.COMPLETED) {
+            paymentEvent =
+                    paymentDomainService.validateAndInitiatePayment(new PaymentServiceDto(payment, creditEntry, creditHistories, failureMessages));
+        } else if (paymentStatus == PaymentStatus.CANCELLED) {
+            paymentEvent =
+                    paymentDomainService.validateAndCancelPayment(new PaymentServiceDto(payment, creditEntry, creditHistories, failureMessages));
+        }
+        persistDBObject(payment, creditEntry, creditHistories, failureMessages);
+        return paymentEvent;
+    }
+
+    private void persistDBObject(Payment payment, CreditEntry creditEntry, List<CreditHistory> creditHistories, List<String> failureMessages) {
+        paymentRepository.save(payment);
+        if (failureMessages.isEmpty()) {
+            creditEntryRepository.save(creditEntry);
+            creditHistoryRepository.save(creditHistories.get(creditHistories.size() - 1));
+        }
+    }
+
+    private List<CreditHistory> getCreditHistories(CustomerId customerId) {
+        Optional<List<CreditHistory>> creditHistories = creditHistoryRepository.findByCustomerId(customerId);
+        if (creditHistories.isEmpty()) {
+            log.error("Could not find credit histories for customer: {}", customerId.getValue());
+            throw new PaymentApplicationServiceException("Could not find credit histories for customer: " + customerId.getValue());
+        }
+        return creditHistories.get();
+    }
+
+    private CreditEntry getCreditEntry(CustomerId customerId) {
+        Optional<CreditEntry> creditEntry = creditEntryRepository.findByCustomerId(customerId);
+        if (creditEntry.isEmpty()) {
+            log.error("Could not find credit entry for customer: {}", customerId.getValue());
+            throw new PaymentApplicationServiceException("Could not find credit entry for customer: " + customerId.getValue());
+        }
+        return creditEntry.get();
+    }
+}
---
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -0,0 +1,21 @@
+package com.food.ordering.system.payment.service.domain;
+
+import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
+import com.food.ordering.system.payment.service.domain.port.input.message.listener.PaymentRequestMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageListener {
+
+    @Override
+    public void completePayment(PaymentRequest paymentRequest) {
+
+    }
+
+    @Override
+    public void cancelPayment(PaymentRequest paymentRequest) {
+
+    }
+}
