1af5437263cef2cca20eb9da86ac52172a567889
MehrdadFalahati <m.falahati2022@gmail.com>
2023-06-15 17:47:41 +0330

    feat: implemented OrderPaymentSaga class.

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.food.ordering.system.order.service.domain.ports.output.repository;
 
+import com.food.ordering.system.domain.valueobject.OrderId;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 
@@ -8,4 +9,5 @@ import java.util.Optional;
 public interface OrderRepository {
     Order save(Order order);
     Optional<Order> findByTrackingId(TrackingId trackingId);
+    Optional<Order> findById(OrderId orderId);
 }
